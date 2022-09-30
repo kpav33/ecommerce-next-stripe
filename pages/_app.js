07482e99c7efa0c557a91bd35ce3accda6f8f7e6
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-09-30 12:01:30 +0200

    Generate product list with getStaticProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Link from "next/link";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
