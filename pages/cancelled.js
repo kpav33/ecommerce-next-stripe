766e250d1e7da3c24d3375b32e9bc3f8f5dbd451
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-10-06 12:14:39 +0200

    Add cancelled page

---
+++ pages/cancelled.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Page from "../components/styled/Page";
+
+export default function Cancelled() {
+  return (
+    <Page>
+      <h2>Payment cancelled!</h2>
+      <p>You have not been charged!</p>
+    </Page>
+  );
+}
